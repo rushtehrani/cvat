d53118d73b6d30eb4539c93f665d089d75d7d6f4
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-07-15 15:29:27 -0700

    Adding class and function for getting "auth-token"

---
+++ cvat/apps/onepanelio/models.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2020 Onepanel Inc.
+#
+# SPDX-License-Identifier: MIT
+
+
+class AuthToken:
+    @staticmethod
+    def get_auth_token(request):
+        return request.session.get("auth-token")
