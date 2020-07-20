bf669511bc484daeff6b7601be52673f4100ab38
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-07-20 12:54:22 -0700

    Changing api_response to JsonResponse.

--- cvat/apps/onepanelio/views.py
+++ cvat/apps/onepanelio/views.py
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: MIT
 
 from __future__ import print_function
+
+from django.http import JsonResponse
+
 from cvat.apps.authentication.decorators import login_required
 from cvat.apps.onepanelio.models import AuthToken
 import time
@@ -10,7 +13,6 @@ import onepanel.core.api
 from onepanel.core.api.rest import ApiException
 from pprint import pprint
 
-@login_required
 def get_workflow_templates(request):
     auth_token = AuthToken.get_auth_token(request)
     configuration = onepanel.core.api.Configuration()
@@ -32,6 +34,6 @@ def get_workflow_templates(request):
     try:
         api_response = api_instance.list_workflow_templates(namespace, page_size=page_size, page=page)
         pprint(api_response)
-        return api_response
+        return JsonResponse(api_response)
     except ApiException as e:
         print("Exception when calling WorkflowTemplateServiceApi->list_workflow_templates: %s\n" % e)
