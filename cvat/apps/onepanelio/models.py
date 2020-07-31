222ae6aada852d1e83d65bb71b3293d672436b33
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-07-30 17:34:25 -0700

    Adding helper function to create_admin_user. - Uses cookie "auth-token" to set password.

--- cvat/apps/onepanelio/models.py
+++ cvat/apps/onepanelio/models.py
@@ -2,8 +2,23 @@
 #
 # SPDX-License-Identifier: MIT
 
+from django.contrib.auth.models import User
+
 
 class AuthToken:
     @staticmethod
     def get_auth_token(request):
         return request.COOKIES['auth-token']
+
+
+class AdminUser:
+    @staticmethod
+    def create_admin_user(request):
+        auth_token = AuthToken.get_auth_token(request)
+        username = "admin"
+        if not User.objects.filter(username=username).exists():
+            u = User(username=username)
+            u.set_password(auth_token)
+            u.is_superuser = True
+            u.is_staff = True
+            u.save()
