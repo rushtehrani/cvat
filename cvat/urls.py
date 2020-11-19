5ddb157f21002adc6cfa643ea4362511168e5432
Aleksandr Melnikov <aleksandr.melnikov@fmailbox.com>
2020-11-19 15:54:33 -0800

    Updating urls. - Removing those that are no longer apps

--- cvat/urls.py
+++ cvat/urls.py
@@ -1,4 +1,3 @@
-
 # Copyright (C) 2018-2019 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
@@ -19,9 +18,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.apps import apps
 from django.contrib import admin
 from django.urls import path, include
-from django.apps import apps
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,33 +30,17 @@ urlpatterns = [
     path('documentation/', include('cvat.apps.documentation.urls')),
 ]
 
-if apps.is_installed('cvat.apps.tf_annotation'):
-    urlpatterns.append(path('tensorflow/annotation/', include('cvat.apps.tf_annotation.urls')))
-
-if apps.is_installed('cvat.apps.git'):
-    urlpatterns.append(path('git/repository/', include('cvat.apps.git.urls')))
-
-if apps.is_installed('cvat.apps.reid'):
-    urlpatterns.append(path('reid/', include('cvat.apps.reid.urls')))
-
-if apps.is_installed('cvat.apps.auto_annotation'):
-    urlpatterns.append(path('auto_annotation/', include('cvat.apps.auto_annotation.urls')))
-
-if apps.is_installed('cvat.apps.dextr_segmentation'):
-    urlpatterns.append(path('dextr/', include('cvat.apps.dextr_segmentation.urls')))
+if apps.is_installed('cvat.apps.dataset_repo'):
+    urlpatterns.append(path('git/repository/', include('cvat.apps.dataset_repo.urls')))
 
 if apps.is_installed('cvat.apps.log_viewer'):
     urlpatterns.append(path('analytics/', include('cvat.apps.log_viewer.urls')))
 
+if apps.is_installed('cvat.apps.lambda_manager'):
+    urlpatterns.append(path('', include('cvat.apps.lambda_manager.urls')))
+
 if apps.is_installed('silk'):
     urlpatterns.append(path('profiler/', include('silk.urls')))
-
-# new feature by Mohammad
-if apps.is_installed('cvat.apps.auto_segmentation'):
-    urlpatterns.append(path('tensorflow/segmentation/', include('cvat.apps.auto_segmentation.urls')))
-
-if apps.is_installed('cvat.apps.tracking'):
-    urlpatterns.append(path('tracking/', include('cvat.apps.tracking.urls')))
 
 # onepanelio
 if apps.is_installed('cvat.apps.onepanelio'):
